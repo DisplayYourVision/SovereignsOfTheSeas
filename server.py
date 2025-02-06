321d49569ad340bcb1c9f1e167ef2fae83bc6f1c
DisplayYourVision <168907771+DisplayYourVision@users.noreply.github.com>
2025-02-06 17:45:47 +0100

    Update server.py

--- server.py
+++ server.py
@@ -3,12 +3,13 @@ import threading
 
 print("boot...")
 
+HOST = "0.0.0.0"
 PORT = 10000
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(ADDR)
+server.bind((HOST, PORT))
 
 def start():
     server.listen()
