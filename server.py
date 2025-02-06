1bbb76a30ef612b06e46c1c796c216294efdaa4b
DisplayYourVision <168907771+DisplayYourVision@users.noreply.github.com>
2025-02-06 17:56:59 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,1 +1,18 @@
-print("testing)
+import socket
+
+print("boot...")
+
+HOST = "0.0.0.0"
+PORT = 10000
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+
+def start():
+    server.listen()
+    while True:
+        conn, addr = server.accept()
+        print(f"Connected by {addr}")
+
+print("connecting..")
+start()
